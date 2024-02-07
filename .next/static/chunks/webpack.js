9d2678b81e1136802ff10574f108da5daa608a43
tbaoiom <taib9642@gmail.com>
2024-02-07 01:02:44 -0700

    another fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "80d2905dbb554ef6"; }
+/******/ 		__webpack_require__.h = function() { return "b4c605a46d498973"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
